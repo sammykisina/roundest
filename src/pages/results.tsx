54d57213acbd0af1833e624135ed47983a6257fd
sam <sammy.k.mutua@gmail.com>
2023-01-08 08:23:19 +0300

    Results Page

---
+++ src/pages/results.tsx
@@ -0,0 +1,80 @@
+import { prisma } from "../server/db";
+import type { GetServerSideProps } from "next";
+import Image from "next/image";
+import React, { type FC } from "react";
+import type { AsyncReturnType } from "src/types/ts-bs";
+
+const getPokemonInOrder = async () => {
+  return await prisma.pokemon.findMany({
+    orderBy: {
+      VoteFor: {
+        _count: "desc",
+      },
+    },
+    select: {
+      id: true,
+      name: true,
+      spriteUrl: true,
+      _count: {
+        select: {
+          VoteFor: true,
+          VoteAgainst: true,
+        },
+      },
+    },
+  });
+};
+
+type PokemonQueryResult = AsyncReturnType<typeof getPokemonInOrder>;
+
+type ResultProps = {
+  pokemon: PokemonQueryResult;
+};
+
+type PokemonListingProps = {
+  pokemon: PokemonQueryResult[number];
+};
+
+const PokemonListing: FC<PokemonListingProps> = ({ pokemon }) => {
+  return (
+    <div className="flex  items-center border-b p-2">
+      <Image src={pokemon.spriteUrl} width={64} height={64} alt="" />
+
+      <div className="capitalize">{pokemon.name}</div>
+    </div>
+  );
+};
+
+const Results: FC<ResultProps> = ({ pokemon }) => {
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="p-4 text-2xl">Results</h2>
+
+      <div className="p-2" />
+
+      <div className="flex w-full max-w-2xl flex-col border">
+        {pokemon?.map((single_pokemon, single_pokemon_index) => {
+          return (
+            <PokemonListing
+              key={single_pokemon_index}
+              pokemon={single_pokemon}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Results;
+
+export const getStaticProps: GetServerSideProps = async () => {
+  const pokemonOrdered = await getPokemonInOrder();
+
+  return {
+    props: {
+      pokemon: pokemonOrdered,
+    },
+    revalidate: 60,
+  };
+};
